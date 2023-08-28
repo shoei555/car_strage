c7342b20275f2dfd1124b521b718c86d0b704f32
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-08-28 15:42:24 +0900

    ルーティング修正#1

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'cars#index'
   resources :cars
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end   
 end
