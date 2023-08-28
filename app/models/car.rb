6ddd4c3fda5d309597b9b9135f4aa83bd7979687
AizawaAkitoshi <137147889+AizawaAkitoshi@users.noreply.github.com>
2023-08-28 17:59:44 +0900

    バリデーションの設定

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,11 @@
 class Car < ApplicationRecord
   with_options presence: true do
     validates :name
-    validates :maker_id
+    validates :maker_id,               numericality: { other_than: 1,message: "を選択してください" }
     validates :model_year
-    validates :mileage 
-    validates :prefecture_id
-    validates :price
+    validates :mileage,                format: { with: /\A[0-9]+\z/ ,message: "が正しくありません"}
+    validates :prefecture_id,          numericality: { other_than: 1,message: "を選択してください" }
+    validates :price,                  numericality: { only_integer: true, greater_than_or_equal_to: 10000, less_than_or_equal_to: 99999999 } # 数字のみuser
     validates :car_code, uniqueness: true
     validates :images,                length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
     #validates :user
