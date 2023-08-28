12f5c52528517ac33f0e4cb040885ce3f254e949
shoei555 <masahide.takakura@glv.co.jp>
2023-08-28 11:40:55 +0900

    Carsコントローラーの作成

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,23 @@
+class CarsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to  cars_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def car_params
+    params.require(:car).permit(:name, :maker_id, :model_year, :mileage, :prefecture_id, :price, :car_code)
+  end
+end
