37a4e5b22b2a50839e5f7d0cadd1bc7781f70770
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-08-28 15:42:50 +0900

    アプリケーションコントローラー修正#1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
-  private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:employee_num,:last_name,:first_name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:employee_num])
   end
-
 end
