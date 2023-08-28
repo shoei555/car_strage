70d564a293b53a4a92a4a9e810d93e86cff8c72d
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-08-28 15:47:10 +0900

    マイグレーション修正#1

--- db/migrate/20230828013219_devise_create_users.rb
+++ db/migrate/20230828013219_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.string :encrypted_password, null: false
       t.integer :employee_num,      null: false
       t.string :last_name,          null: false
-      t.string :first_name,        null: false
+      t.string :first_name,         null: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -38,9 +38,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
 
       t.timestamps null: false
     end
-
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :employee_num,         unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
