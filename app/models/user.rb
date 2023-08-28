c8b094e08908add398620fd803b03eefdb2dd317
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-08-28 15:46:40 +0900

    user モデル修正#2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :validatable,:rememberable
+  
+  with_options presence: true do
+    validates :employee_num, format: {with:/[0-9]{6}/}
+    validates :last_name,    format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/}
+    validates :first_name,   format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/}
+  end
+
+  with_options uniqueness: true do
+    validates :employee_num
+  end
+
 end
