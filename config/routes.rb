c6f7e3bd4f2e3c2ab22b3414334d98eefbc47e44
AizawaAkitoshi <137147889+AizawaAkitoshi@users.noreply.github.com>
2023-08-29 15:55:51 +0900

    詳細の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'cars#index'
-  resources :cars, only: [:index, :new, :create]
+  resources :cars, only: [:index, :new, :create, :show]
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
