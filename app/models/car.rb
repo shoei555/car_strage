3da76bc4e694810b38c4c0f8106e8eea85fa2c7b
本津　慧人 <137137801+keitootiek@users.noreply.github.com>
2023-08-31 10:37:51 +0900

    モデルにバリデーション追記

--- app/models/car.rb
+++ app/models/car.rb
@@ -8,6 +8,11 @@ class Car < ApplicationRecord
     validates :price,                  numericality: { only_integer: true, greater_than_or_equal_to: 10000, less_than_or_equal_to: 99999999 } # 数字のみ
     validates :car_code, uniqueness: true
     validates :images,                length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
+    validates :car_inspection                                                  #車検期間
+    validates :displacement                                                    #排気量
+    validates :car_color                                                       #車体色
+    validates :car_fuel                                                        #燃料
+    validates :wheel_drive                                                     #駆動方式
     validates :user_id
   end
   
