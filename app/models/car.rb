6055d3248f7517ef45bea117629049d62c3d7cf1
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-09-05 10:25:23 +0900

    モデル変更(画像の制限枚数変更)

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,12 +5,12 @@ class Car < ApplicationRecord
     validates :model_year
     validates :mileage,                numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 1000000 } # 数字のみ
     validates :prefecture_id,          numericality: { other_than: 1,message: "を選択してください" }
-    validates :price,                  numericality: { only_integer: true, greater_than_or_equal_to: 10000, less_than_or_equal_to: 99999999 } # 数字のみ
+    validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 9999,allow_float: true }
     validates :car_code, uniqueness: true
-    validates :images,                length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
+    validates :images,                length: { minimum: 1, maximum: 6, message: "は1枚以上6枚以下にしてください" }
     validates :car_inspection                                                  #車検期間
     validates :displacement                                                    #排気量
-    validates :car_color                                                       #車体色
+    validates :car_color                                                      #車体色
     validates :car_fuel                                                        #燃料
     validates :wheel_drive                                                     #駆動方式
     validates :user_id
@@ -24,4 +24,8 @@ class Car < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :maker
+  belongs_to :color
+  belongs_to :wheeldrive
+  belongs_to :carfuel
+  belongs_to :tag
 end
