687214c818d966e0ad2b47d2168a32e6511be3cf
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-09-05 10:24:26 +0900

    アクティブハッシュデータ追加

---
+++ app/models/color.rb
@@ -0,0 +1,24 @@
+class Color < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: 'パール' },
+    { id: 3, name: 'ホワイト' },
+    { id: 4, name: 'ブラック' },
+    { id: 5, name: 'レッド' },
+    { id: 6, name: 'ブルー' },
+    { id: 7, name: 'ライトブルー' },
+    { id: 8, name: 'グリーン' },
+    { id: 9, name: 'ライトグリーン' },
+    { id: 10, name: 'シルバー' },
+    { id: 11, name: 'ゴールド' },
+    { id: 12, name: 'グレー' },
+    { id: 13, name: 'イエロー' },
+    { id: 14, name: 'パープル' },
+    { id: 15, name: 'アイボリー' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :cars
+
+  
+end
---
+++ app/models/wheeldrive.rb
@@ -0,0 +1,11 @@
+class Wheeldrive < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '2WD' },
+    { id: 3, name: '4WD' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :cars
+
+end
