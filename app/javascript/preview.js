48d2729ebb2df4165a03ca01b90cfa064ab1a0b6
本津　慧人 <137137801+keitootiek@users.noreply.github.com>
2023-09-06 15:43:42 +0900

    新規登録画面変更・編集画面実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,4 +1,4 @@
-document.addEventListener('turbo:load', function () {
+document.addEventListener('turbo:load', function(){
   // 新規投稿・編集ページのフォームを取得
   const postForm = document.getElementById('new_car');
   // プレビューを表示するためのスペースを取得
@@ -6,88 +6,116 @@ document.addEventListener('turbo:load', function () {
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!postForm) return null;
 
-  // 投稿できる枚数の制限を定義
-  const imageLimits = 6;
-
-  // input要素を取得
-  const fileField = document.querySelector('input[type="file"][name="car[images][]"]');
-  const fileFieldContainer = fileField.parentElement;
-
-  // input要素で値の変化が起きた際に呼び出される関数の中身
-  const changedFileField = (e) => {
-    const files = e.target.files;
-
-    // 選択されたファイルを順番に処理
-    for (let i = 0; i < files.length; i++) {
-      const dataIndex = document.querySelectorAll(".preview").length;
-      const file = files[i];
-
-      // fileが空 = 何も選択しなかったので終了
-      if (!file) {
-        return null;
-      };
-
-      const blob = window.URL.createObjectURL(file);
-
-      // 画像をプレビューに表示
-      buildPreviewImage(dataIndex, blob);
-      
-
-      // 画像の枚数が最大のときに非表示にする
-      if (document.querySelectorAll(".preview").length >= imageLimits) {
-        fileFieldContainer.style.display = 'none';
-      }
-    }
-  };
-
-  
-
-  // input要素で値の変化が起きた際に呼び出される関数
-  fileField.addEventListener('change', changedFileField);
+   // 投稿できる枚数の制限を定義
+    const imageLimits = 6;
 
   // プレビュー画像を生成・表示する関数
-  const buildPreviewImage = (dataIndex, blob) => {
+  const buildPreviewImage = (dataIndex, blob) =>{
     // 画像を表示するためのdiv要素を生成
     const previewWrapper = document.createElement('div');
     previewWrapper.setAttribute('class', 'preview');
     previewWrapper.setAttribute('data-index', dataIndex);
 
     // 表示する画像を生成
-    const previewImage = document.createElement('img');
+    const previewImage= document.createElement('img');
     previewImage.setAttribute('class', 'preview-image');
     previewImage.setAttribute('src', blob);
-    previewImage.setAttribute('width', '200'); // 幅を指定
-    previewImage.setAttribute('height', '150'); // 高さを指定
-
+    previewImage.setAttribute('width', '150'); // 幅を指定
+    previewImage.setAttribute('height', '200');// 高さを指定
+    
     // 削除ボタンを生成
     const deleteButton = document.createElement("div");
     deleteButton.setAttribute("class", "image-delete-button");
     deleteButton.innerText = "削除";
-    deleteButton.style.width = "45px"; // 幅を指定
-    deleteButton.style.height = "30px"; // 高さを指定
-    deleteButton.style.borderRadius = "20%"; // 丸くするための角丸の半径
-    deleteButton.style.backgroundColor = "black"; // 背景色を赤色に設定
-    deleteButton.style.color = "white"; // 白色の文字色に変更
+    deleteButton.style.textAlign = "center";
+    deleteButton.style.width = "60px"; // 幅を指定
+    deleteButton.style.height = "40px"; // 高さを指定
+    deleteButton.style.color = "white";
+    deleteButton.style.backgroundColor = "black";
+    deleteButton.style.borderRadius = "30%"; // 丸くするための角丸の半径
+    deleteButton.style.display = "flex";
+    deleteButton.style.alignItems = "center";
+    deleteButton.style.justifyContent = "center";
 
-    // 削除ボタンをクリックしたらプレビューを削除
+    // 削除ボタンをクリックしたらプレビューとfile_fieldを削除させる
     deleteButton.addEventListener("click", () => deleteImage(dataIndex));
 
     // 生成したHTMLの要素をブラウザに表示させる
     previewWrapper.appendChild(previewImage);
     previewWrapper.appendChild(deleteButton);
     previewList.appendChild(previewWrapper);
-
-    
   };
 
+  // file_fieldを生成・表示する関数
+  const buildNewFileField = () => {
+    // 2枚目用のfile_fieldを作成
+    const newFileField = document.createElement('input');
+    newFileField.setAttribute('type', 'file');
+    newFileField.setAttribute('name', 'car[images][]');
+    newFileField.style.width = "280px"; 
+    newFileField.style.height = "30px";
+  
+
+    // 最後のfile_fieldを取得
+    const lastFileField = document.querySelector('input[type="file"][name="car[images][]"]:last-child');
+    // nextDataIndex = 最後のfile_fieldのdata-index + 1
+    const nextDataIndex = Number(lastFileField.getAttribute('data-index')) +1;
+    newFileField.setAttribute('data-index', nextDataIndex);
+
+    // 追加されたfile_fieldにchangeイベントをセット
+    newFileField.addEventListener("change", changedFileField);
+
+    // 生成したfile_fieldを表示
+    const fileFieldsArea = document.querySelector('.click-upload');
+    fileFieldsArea.appendChild(newFileField);
+  };
   // 指定したdata-indexを持つプレビューとfile_fieldを削除する
   const deleteImage = (dataIndex) => {
     const deletePreviewImage = document.querySelector(`.preview[data-index="${dataIndex}"]`);
     deletePreviewImage.remove();
+    const deleteFileField = document.querySelector(`input[type="file"][data-index="${dataIndex}"]`);
+    deleteFileField.remove();
 
-    // 画像の枚数が最大未満の場合、file_fieldを再表示
-    if (document.querySelectorAll(".preview").length < imageLimits) {
-      fileFieldContainer.style.display = 'flex';
-    }
+    // 画像の枚数が最大のときに削除ボタンを押した場合、file_fieldを1つ追加する
+    const imageCount = document.querySelectorAll(".preview").length;
+    if (imageCount == imageLimits - 1) buildNewFileField();
   };
+
+  // input要素で値の変化が起きた際に呼び出される関数の中身
+  const changedFileField = (e) => {
+    // data-index（何番目を操作しているか）を取得
+    const dataIndex = e.target.getAttribute('data-index');
+
+    const file = e.target.files[0];
+
+    // fileが空 = 何も選択しなかったのでプレビュー等を削除して終了する
+    if (!file) {
+      deleteImage(dataIndex);
+      return null;
+    };
+
+    const blob = window.URL.createObjectURL(file);
+
+    // data-indexを使用して、既にプレビューが表示されているかを確認する
+    const alreadyPreview = document.querySelector(`.preview[data-index="${dataIndex}"]`);
+
+    if (alreadyPreview) {
+      // クリックしたfile_fieldのdata-indexと、同じ番号のプレビュー画像が既に表示されている場合は、画像の差し替えのみを行う
+      const alreadyPreviewImage = alreadyPreview.querySelector("img");
+      alreadyPreviewImage.setAttribute("src", blob);
+      return null;
+    };
+
+    buildPreviewImage(dataIndex, blob);
+
+    // 画像の枚数制限に引っかからなければ、新しいfile_fieldを追加する
+    const imageCount = document.querySelectorAll(".preview").length;
+    if (imageCount < imageLimits) buildNewFileField();
+  };
+
+  // input要素を取得
+  const fileField = document.querySelector('input[type="file"][name="car[images][]"]');
+
+  // input要素で値の変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', changedFileField);
 });
